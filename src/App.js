0e5f65b75ce4bcd20417caaf8ac12cdbfbdb8f21
unknown <muni818p@gmail.com>
2023-10-15 22:18:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      React-tasks
+    </h1>
+      <table>
+        <tr>
+          <th>
+            Tasks
+          </th>
+          <th>
+            Github-repo
+          </th>
+          <th>
+            deployed-link
+          </th>
+        </tr>
+        <tr>
+          <td>
+          Task-1
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/Mernreact1">Github-repo-1</a>
+          </td>
+          <td>
+            <a href="https://mern-1-munikumar818.netlify.app/">Link-1</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          Task-2
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/calculator">Github-repo-2</a>
+          </td>
+          <td>
+            <a href="https://818-calculator.netlify.app/">Link-2</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          Task-3
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/mernreact3">Github-repo-3</a>
+          </td>
+          <td>
+            <a href="https://colorpicker-818.netlify.app/">Link-3</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          Task-4
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/4">Github-repo-4</a>
+          </td>
+          <td>
+            <a href="https://dummy-json-818.netlify.app/">Link-4</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          Task-5
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/5">Github-repo-5</a>
+          </td>
+          <td>
+            <a href="https://age-calculator-818.netlify.app/">Link-5</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          Task-6
+          </td>
+          <td>
+            <a href="https://github.com/Munikumar818/Mywebsite">Github-repo-6</a>
+          </td>
+          <td>
+            <a href="https://muni818.netlify.app/">Link-6</a>
+          </td>
+        </tr>
+
+        
+      </table>
+    </div>
+  );
+}
+
+export default App;
